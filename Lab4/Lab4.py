1a45d2750c076320c5ea02cfba13247ba31df12a
Kevin Peng <kevinpkf@gmail.com>
2022-10-13 14:41:36 -0700

    fix

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -28,9 +28,28 @@ def sortingWorker(firstHalf: bool) -> None:
       leftArray = mergesort(array[:center])
       rightArray = mergesort(array[center:])
 
-      # use shared mergeStepHelper for merging sorted arrays
-      return mergeStepHelper(leftArray, rightArray)
+      # sorted merged array
+      sortedMerge = []
 
+      # used to keep track of current index when adding to sortedMerge array from leftArray and rightArray
+      leftIndex = 0
+      rightIndex = 0
+
+      # while both arrays are not empty, add lowest current value from either array using current indices
+      while leftIndex < len(leftArray) and rightIndex < len(rightArray):
+        if leftArray[leftIndex] < rightArray[rightIndex]:
+          sortedMerge.append(leftArray[leftIndex])
+          leftIndex += 1
+        else:
+          sortedMerge.append(rightArray[rightIndex])
+          rightIndex += 1
+      
+      # whatever is left in either leftArray or rightArray starting from the remaining indices will be sorted and greater than our sortedMerge array
+      sortedMerge.extend(leftArray[leftIndex:])
+      sortedMerge.extend(rightArray[rightIndex:])
+
+      return sortedMerge
+    # end mergesort implementation
 
     center = len(testcase) // 2
 
@@ -45,36 +64,31 @@ def mergingWorker() -> None:
         them into a single sorted list that is stored in
         the shared variable sortedFullList.
     """
-    SortedFullList.extend(mergeStepHelper(sortedFirstHalf, sortedSecondHalf))
+    # used to keep track of current index when adding to sortedMerge array from leftArray and rightArray
+    leftIndex = 0
+    rightIndex = 0
 
-# IMPORTANT: Assumes leftArray and rightArray are sorted already
-# does not mutate left and right arrays
-def mergeStepHelper(leftArray, rightArray):
-  # sorted merged array
-  sortedMerge = []
+    # while both arrays are not empty, add lowest current value from either array using current indices
+    while leftIndex < len(sortedFirstHalf) and rightIndex < len(sortedSecondHalf):
+      if sortedFirstHalf[leftIndex] < sortedSecondHalf[rightIndex]:
+        SortedFullList.append(sortedFirstHalf[leftIndex])
+        leftIndex += 1
+      else:
+        SortedFullList.append(sortedSecondHalf[rightIndex])
+        rightIndex += 1
+    
+    # whatever is left in either leftArray or rightArray starting from the remaining indices will be sorted and greater than our sortedMerge array
+    SortedFullList.extend(sortedFirstHalf[leftIndex:])
+    SortedFullList.extend(sortedSecondHalf[rightIndex:])
 
-  # used to keep track of current index when adding to sortedMerge array from leftArray and rightArray
-  leftIndex = 0
-  rightIndex = 0
-
-  # while both arrays are not empty, add lowest current value from either array using current indices
-  while leftIndex < len(leftArray) and rightIndex < len(rightArray):
-    if leftArray[leftIndex] < rightArray[rightIndex]:
-      sortedMerge.append(leftArray[leftIndex])
-      leftIndex += 1
-    else:
-      sortedMerge.append(rightArray[rightIndex])
-      rightIndex += 1
-  
-  # whatever is left in either leftArray or rightArray starting from the remaining indices will be sorted and greater than our sortedMerge array
-  sortedMerge.extend(leftArray[leftIndex:])
-  sortedMerge.extend(rightArray[rightIndex:])
-
-  return sortedMerge
 
 if __name__ == "__main__":
     #shared variables
     testcase = [8,5,7,7,4,1,3,2]
+    testcase1 = []
+    testcase2 = [2]
+    testcase3 = [8,-5,7,7,4,1,3,-2,100]
+
     sortedFirstHalf: list = []
     sortedSecondHalf: list = []
     SortedFullList: list = []
